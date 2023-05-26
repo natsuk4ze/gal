665845aaa77a8cfdf39838eeaa739aeff46eb852
yuoohama <negotoyolo@gmail.com>
2023-05-26 21:14:21 +0900

    Refactor

--- android/src/main/java/studio/midoridesign/gal/GalPlugin.java
+++ android/src/main/java/studio/midoridesign/gal/GalPlugin.java
@@ -24,12 +24,6 @@ import java.util.concurrent.CompletableFuture;
 
 /** GalPlugin */
 public class GalPlugin implements FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native
-  /// Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine
-  /// and unregister it
-  /// when the Flutter Engine is detached from the Activity
   private MethodChannel channel;
   private FlutterPluginBinding pluginBinding;
 
@@ -67,21 +61,14 @@ public class GalPlugin implements FlutterPlugin, MethodCallHandler {
   private void putVideo(Context context, String path) throws IOException {
     ContentResolver resolver = context.getContentResolver();
     ContentValues values = new ContentValues();
-
-    // Set the file name
     File file = new File(path);
+
     values.put(MediaStore.Video.Media.DISPLAY_NAME, file.getName());
-
-    // Set the mime type
     values.put(MediaStore.Video.Media.MIME_TYPE, "video/mp4");
-
-    // Set the date taken
     values.put(MediaStore.Video.Media.DATE_TAKEN, System.currentTimeMillis());
 
-    // Insert the metadata into the MediaStore
     Uri videoUri = resolver.insert(MediaStore.Video.Media.EXTERNAL_CONTENT_URI, values);
 
-    // Copy the video data into the MediaStore
     try (OutputStream out = resolver.openOutputStream(videoUri);
         InputStream in = new FileInputStream(file)) {
       byte[] buffer = new byte[8192];
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,10 +1,9 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-
 import 'package:flutter/services.dart';
+
 import 'package:gal/gal.dart';
-import 'package:path_provider/path_provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,29 +15,40 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(useMaterial3: true),
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: Colors.green,
+      ),
       home: Scaffold(
+        appBar: AppBar(
+          title: Center(
+              child: Text(
+            "Gal Example 💚",
+            style: Theme.of(context).textTheme.titleLarge,
+          )),
+        ),
         body: Center(
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
-              FilledButton(
-                onPressed: () async {
-                  Gal.open();
-                },
-                child: const Text('Open Gallery'),
+              FilledButton.icon(
+                onPressed: () async => Gal.open(),
+                label: const Text('Open Gallery'),
+                icon: const Icon(Icons.open_in_new),
               ),
-              FilledButton(
+              FilledButton.icon(
                 onPressed: () async {
                   final byteData = await rootBundle.load('assets/done.mp4');
-
-                  final tempDir = await getTemporaryDirectory();
-                  final file = await File('${tempDir.path}/video.mp4').create();
+                  final file =
+                      await File('${Directory.systemTemp.path}/done.mp4')
+                          .create();
                   await file.writeAsBytes(byteData.buffer.asUint8List(
                       byteData.offsetInBytes, byteData.lengthInBytes));
-                  Gal.putVideo(file.path);
+
+                  await Gal.putVideo(file.path);
                 },
-                child: const Text('Put Video'),
+                label: const Text('Save Video'),
+                icon: const Icon(Icons.download),
               ),
             ],
           ),
