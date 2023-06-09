12c1b286e0f5c5092e6aa9446cb851bdd81d2f5b
YuOohama <negotoyolo@gmail.com>
2023-06-09 21:03:11 +0900

    Add handle exceptions

--- android/src/main/java/studio/midoridesign/gal/GalPlugin.java
+++ android/src/main/java/studio/midoridesign/gal/GalPlugin.java
@@ -18,6 +18,7 @@ import android.provider.MediaStore;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -36,26 +37,32 @@ public class GalPlugin implements FlutterPlugin, MethodCallHandler {
 
     @Override
     public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
-        if (call.method.equals("putVideo")) {
+        if (call.method.equals("putVideo") || call.method.equals("putImage")) {
             String path = call.argument("path");
+            Uri contentUri = call.method.equals("putVideo") ? MediaStore.Video.Media.EXTERNAL_CONTENT_URI : MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+
             CompletableFuture.runAsync(() -> {
                 try {
-                    putVideo(pluginBinding.getApplicationContext(), path);
+                    putMedia(pluginBinding.getApplicationContext(), path, contentUri);
                     new Handler(Looper.getMainLooper()).post(() -> result.success(null));
-                } catch (IOException e) {
+                } 
+                catch (SecurityException e) {
                     new Handler(Looper.getMainLooper())
-                            .post(() -> result.error("IO_ERROR", "Failed to save video to gallery", null));
+                            .post(() -> result.error("ACCESS_DENIED", null, null));
                 }
-            });
-        } else if (call.method.equals("putImage")) {
-            String path = call.argument("path");
-            CompletableFuture.runAsync(() -> {
-                try {
-                    putImage(pluginBinding.getApplicationContext(), path);
-                    new Handler(Looper.getMainLooper()).post(() -> result.success(null));
-                } catch (IOException e) {
+                catch (FileNotFoundException e) {
                     new Handler(Looper.getMainLooper())
-                            .post(() -> result.error("IO_ERROR", "Failed to save image to gallery", null));
+                            .post(() -> result.error("NOT_SUPPORTED_FORMAT", null, null));
+                }
+                catch (IOException e) {
+                    String message = e.getMessage();
+                    if (message != null && message.contains("No space left on device")) {
+                        new Handler(Looper.getMainLooper())
+                            .post(() -> result.error("NOT_ENOUGH_SPACE", null, null));
+                    } else {
+                        new Handler(Looper.getMainLooper())
+                            .post(() -> result.error("UNEXPECTED", null, null));
+                    }            
                 }
             });
         } else {
@@ -69,37 +76,17 @@ public class GalPlugin implements FlutterPlugin, MethodCallHandler {
         pluginBinding = null;
     }
 
-    private void putVideo(Context context, String path) throws IOException {
+    private void putMedia(Context context, String path, Uri contentUri) throws IOException, SecurityException, FileNotFoundException {
         ContentResolver resolver = context.getContentResolver();
         ContentValues values = new ContentValues();
         File file = new File(path);
 
-        values.put(MediaStore.Video.Media.DISPLAY_NAME, file.getName());
-        values.put(MediaStore.Video.Media.DATE_TAKEN, System.currentTimeMillis());
+        values.put(MediaStore.MediaColumns.DISPLAY_NAME, file.getName());
+        values.put(MediaStore.MediaColumns.DATE_ADDED, System.currentTimeMillis());
 
-        Uri videoUri = resolver.insert(MediaStore.Video.Media.EXTERNAL_CONTENT_URI, values);
+        Uri mediaUri = resolver.insert(contentUri, values);
 
-        try (OutputStream out = resolver.openOutputStream(videoUri);
-             InputStream in = new FileInputStream(file)) {
-            byte[] buffer = new byte[8192];
-            int bytesRead;
-            while ((bytesRead = in.read(buffer)) != -1) {
-                out.write(buffer, 0, bytesRead);
-            }
-        }
-    }
-
-    private void putImage(Context context, String path) throws IOException {
-        ContentResolver resolver = context.getContentResolver();
-        ContentValues values = new ContentValues();
-        File file = new File(path);
-
-        values.put(MediaStore.Images.Media.DISPLAY_NAME, file.getName());
-        values.put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis());
-
-        Uri imageUri = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
-
-        try (OutputStream out = resolver.openOutputStream(imageUri);
+        try (OutputStream out = resolver.openOutputStream(mediaUri);
              InputStream in = new FileInputStream(file)) {
             byte[] buffer = new byte[8192];
             int bytesRead;
