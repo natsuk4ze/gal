7f517496dada76b79dc1d11459440dad3f431ff3
YuOohama <negotoyolo@gmail.com>
2023-06-10 17:25:51 +0900

    Add docs

--- lib/gal.dart
+++ lib/gal.dart
@@ -7,23 +7,25 @@ import 'gal_platform_interface.dart';
 class Gal {
   /// Save video to native gallery app
   /// [path] is local path.
-  /// not supported [Uri] or something.
   static Future<void> putVideo(String path) async =>
       _voidOrThrow(() async => GalPlatform.instance.putVideo(path));
 
   /// Save image to native gallery app
   /// [path] is local path.
-  /// not supported [Uri] or something.
   static Future<void> putImage(String path) async =>
       _voidOrThrow(() async => GalPlatform.instance.putImage(path));
 
   /// Open "iOS Photos" when iOS, "Google Photos" when Android.
   static Future<void> open() async => GalPlatform.instance.open();
-
+  
+  /// Returns whether or not the app has access.
+  /// For Android API >=29, or <23, this request is unnecessary and returns true immediately.
   static Future<bool> hasAccess() async => GalPlatform.instance.hasAccess();
 
-  /// On IOS, once the user rejects it, the dialog cannot be displayed and 
-  /// returns false immediately
+  /// Displays a dialog for the request and returns whether the user has accepted.
+  /// On iOS, once the user rejects it, the dialog cannot be displayed and 
+  /// returns false immediately.
+  /// For Android API >=29, or <23, this request is unnecessary and returns true immediately.
   static Future<bool> requestAccess() async => GalPlatform.instance.requestAccess();
 
   static Future<void> _voidOrThrow(Future<void> Function() cb) async {
