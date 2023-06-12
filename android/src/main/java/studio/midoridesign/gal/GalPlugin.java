65d9c267c4e6076ba75be3716ed7caa2374f6af8
YuOohama <negotoyolo@gmail.com>
2023-06-13 00:20:57 +0900

    Refactor java code

--- android/src/main/java/studio/midoridesign/gal/GalPlugin.java
+++ android/src/main/java/studio/midoridesign/gal/GalPlugin.java
@@ -1,29 +1,29 @@
 package studio.midoridesign.gal;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+import androidx.core.app.ActivityCompat;
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.embedding.engine.plugins.activity.ActivityAware;
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
+import io.flutter.plugin.common.PluginRegistry;
 
 import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Handler;
 import android.os.Looper;
+import android.os.Build;
 import android.provider.MediaStore;
 import android.content.Intent;
 import android.Manifest;
-import android.content.pm.PackageManager;
-import android.os.Build;
-import androidx.core.content.ContextCompat;
-import androidx.core.app.ActivityCompat;
-import io.flutter.embedding.engine.plugins.activity.ActivityAware;
-import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
-import io.flutter.plugin.common.PluginRegistry;
 import android.app.Activity;
 
 import java.io.File;
@@ -34,7 +34,8 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.concurrent.CompletableFuture;
 
-public class GalPlugin implements FlutterPlugin, MethodCallHandler, ActivityAware, PluginRegistry.RequestPermissionsResultListener {
+public class GalPlugin
+        implements FlutterPlugin, MethodCallHandler, ActivityAware, PluginRegistry.RequestPermissionsResultListener {
     private MethodChannel channel;
     private FlutterPluginBinding pluginBinding;
     private static final String PERMISSION = Manifest.permission.WRITE_EXTERNAL_STORAGE;
@@ -52,64 +53,69 @@ public class GalPlugin implements FlutterPlugin, MethodCallHandler, ActivityAwar
     @Override
     public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
         switch (call.method) {
-        case "putVideo":
-        case "putImage": {
-            String path = call.argument("path");
-            Uri contentUri = call.method.equals("putVideo") ? MediaStore.Video.Media.EXTERNAL_CONTENT_URI : MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+            case "putVideo":
+            case "putImage": {
+                String path = call.argument("path");
+                Uri contentUri = call.method.equals("putVideo") ? MediaStore.Video.Media.EXTERNAL_CONTENT_URI
+                        : MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
 
-            CompletableFuture.runAsync(() -> {
-                try {
-                    putMedia(pluginBinding.getApplicationContext(), path, contentUri);
-                    new Handler(Looper.getMainLooper()).post(() -> result.success(null));
-                } 
-                catch (SecurityException e) {
-                    new Handler(Looper.getMainLooper())
-                            .post(() -> result.error("ACCESS_DENIED", null, null));
-                }
-                catch (FileNotFoundException e) {
-                    new Handler(Looper.getMainLooper())
-                            .post(() -> result.error("NOT_SUPPORTED_FORMAT", null, null));
-                }
-                catch (IOException e) {
-                    String message = e.getMessage();
-                    if (message != null && message.contains("No space left on device")) {
-                        new Handler(Looper.getMainLooper())
-                            .post(() -> result.error("NOT_ENOUGH_SPACE", null, null));
-                    } else {
-                        new Handler(Looper.getMainLooper())
-                            .post(() -> result.error("UNEXPECTED", null, null));
-                    }            
-                }
-            });
-            break;
-        }
-        case "open": {
-            open();
-            new Handler(Looper.getMainLooper())
+                CompletableFuture.runAsync(() -> {
+                    try {
+                        putMedia(pluginBinding.getApplicationContext(), path, contentUri);
+                        new Handler(Looper.getMainLooper()).post(() -> result.success(null));
+                    } catch (SecurityException e) {
+                        sendError("ACCESS_DENIED", e.toString(), e.getStackTrace(), result);
+                    } catch (FileNotFoundException e) {
+                        sendError("NOT_SUPPORTED_FORMAT", e.toString(), e.getStackTrace(), result);
+                    } catch (IOException e) {
+                        String message = e.toString();
+                        if (message != null && message.contains("No space left on device")) {
+                            sendError("NOT_ENOUGH_SPACE", e.toString(), e.getStackTrace(), result);
+                        } else {
+                            sendError("UNEXPECTED", e.toString(), e.getStackTrace(), result);
+                        }
+                    }
+                });
+                break;
+            }
+            case "open": {
+                open();
+                new Handler(Looper.getMainLooper())
                         .post(() -> result.success(null));
-            break;
+                break;
+            }
+            case "hasAccess": {
+                result.success(hasAccess());
+                break;
+            }
+            case "requestAccess": {
+                if (hasAccess()) {
+                    result.success(true);
+                    return;
+                }
+                accessRequestResult = new CompletableFuture<>();
+                ActivityCompat.requestPermissions(activity, new String[] { PERMISSION }, PERMISSION_REQUEST_CODE);
+                accessRequestResult.thenAccept(result::success);
+                break;
+            }
+            default: {
+                result.notImplemented();
+            }
         }
-        case "hasAccess": {
-            result.success(hasAccess());
-            break;
-        }
-        case "requestAccess": {
-            if (hasAccess()) {
-                result.success(true);
-                return;
-            } 
-            accessRequestResult = new CompletableFuture<>();
-            ActivityCompat.requestPermissions(activity, new String[]{PERMISSION}, PERMISSION_REQUEST_CODE);
-            accessRequestResult.thenAccept(result::success);
-            break;
-        }
-        default: {
-            result.notImplemented();
-        }
-    }
     }
 
-    private void putMedia(Context context, String path, Uri contentUri) throws IOException, SecurityException, FileNotFoundException {
+    private void sendError(String errorCode, String message, StackTraceElement[] stackTrace, Result result) {
+        StringBuilder trace = new StringBuilder();
+        for (StackTraceElement st : stackTrace) {
+            trace.append(st.toString());
+            trace.append("\n");
+        }
+        new Handler(Looper.getMainLooper())
+                .post(() -> result.error(errorCode, message, trace.toString()));
+    }
+
+    private void putMedia(Context context, String path, Uri contentUri)
+            throws IOException, SecurityException, FileNotFoundException {
         ContentResolver resolver = context.getContentResolver();
         ContentValues values = new ContentValues();
         File file = new File(path);
@@ -120,7 +126,7 @@ public class GalPlugin implements FlutterPlugin, MethodCallHandler, ActivityAwar
         Uri mediaUri = resolver.insert(contentUri, values);
 
         try (OutputStream out = resolver.openOutputStream(mediaUri);
-             InputStream in = new FileInputStream(file)) {
+                InputStream in = new FileInputStream(file)) {
             byte[] buffer = new byte[8192];
             int bytesRead;
             while ((bytesRead = in.read(buffer)) != -1) {
@@ -129,7 +135,7 @@ public class GalPlugin implements FlutterPlugin, MethodCallHandler, ActivityAwar
         }
     }
 
-    private void open(){
+    private void open() {
         Context context = pluginBinding.getApplicationContext();
         Intent intent = new Intent();
         intent.setAction(Intent.ACTION_VIEW);
@@ -172,8 +178,9 @@ public class GalPlugin implements FlutterPlugin, MethodCallHandler, ActivityAwar
 
     @Override
     public boolean onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        accessRequestResult.complete(grantResults[0] == PackageManager.PERMISSION_GRANTED && PERMISSION_REQUEST_CODE == requestCode);
-        return true;   
+        accessRequestResult.complete(
+                grantResults[0] == PackageManager.PERMISSION_GRANTED && PERMISSION_REQUEST_CODE == requestCode);
+        return true;
     }
 
     @Override
