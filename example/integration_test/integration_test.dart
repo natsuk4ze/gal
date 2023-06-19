85329e7c87632bd7b4af3eab8af90fe10a516ce0
YuOohama <negotoyolo@gmail.com>
2023-06-19 15:27:08 +0900

    Refactor

--- example/integration_test/integration_test.dart
+++ example/integration_test/integration_test.dart
@@ -9,45 +9,61 @@ import '../lib/main.dart' as app;
 
 /// All tests are done in integration tests,
 /// since only integration tests can call native code.
-/// Other functions take longer to implement
-/// because of the possibility of interacting with native dialogs.
-/// For more info: https://github.com/flutter/flutter/wiki/Plugin-Tests
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  Platform.isAndroid
+      ? group('Android Test', () {
+          testWidgets('hasAccess()', (tester) async {
+            app.main();
+            await tester.pumpAndSettle();
+            final button = find.byIcon(Icons.question_mark);
+            await tester.tap(button);
+            expect(tester.takeException(), isNull);
+          });
 
-  group('Test', () {
-    testWidgets('hasAccess()', (tester) async {
-      app.main();
-      await tester.pumpAndSettle();
-      final button = find.byIcon(Icons.question_mark);
-      await tester.tap(button);
-      expect(tester.takeException(), isNull);
-    });
+          testWidgets('putVideo()', (tester) async {
+            app.main();
+            await tester.pumpAndSettle();
+            final button = find.byIcon(Icons.video_file);
+            await tester.tap(button);
+            expect(tester.takeException(), isNull);
+          });
 
-    testWidgets('open()', (tester) async {
-      app.main();
-      await tester.pumpAndSettle();
-      final button = find.byIcon(Icons.open_in_new);
-      await tester.tap(button);
-      expect(tester.takeException(), isNull);
-    });
+          testWidgets('putImage()', (tester) async {
+            app.main();
+            await tester.pumpAndSettle();
+            final button = find.byIcon(Icons.image);
+            await tester.tap(button);
+            expect(tester.takeException(), isNull);
+          });
 
-    if (Platform.isIOS) return;
+          testWidgets('open()', (tester) async {
+            app.main();
+            await tester.pumpAndSettle();
+            final button = find.byIcon(Icons.open_in_new);
+            await tester.tap(button);
+            expect(tester.takeException(), isNull);
+          });
+        })
+      : group('iOS Test', () {
+          testWidgets('hasAccess()', (tester) async {
+            app.main();
+            await tester.pumpAndSettle();
+            final button = find.byIcon(Icons.question_mark);
+            await tester.tap(button);
+            expect(tester.takeException(), isNull);
+          });
 
-    testWidgets('putVideo()', (tester) async {
-      app.main();
-      await tester.pumpAndSettle();
-      final button = find.byIcon(Icons.video_file);
-      await tester.tap(button);
-      expect(tester.takeException(), isNull);
-    });
+          testWidgets('open()', (tester) async {
+            app.main();
+            await tester.pumpAndSettle();
+            final button = find.byIcon(Icons.open_in_new);
+            await tester.tap(button);
+            expect(tester.takeException(), isNull);
+          });
 
-    testWidgets('putImage()', (tester) async {
-      app.main();
-      await tester.pumpAndSettle();
-      final button = find.byIcon(Icons.image);
-      await tester.tap(button);
-      expect(tester.takeException(), isNull);
-    });
-  });
+          /// Other functions take longer to implement
+          /// because of the possibility of interacting with native dialogs.
+          /// For more info: https://github.com/flutter/flutter/wiki/Plugin-Tests
+        });
 }
