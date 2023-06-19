d4529c9176c200c1de3e14716ea9653e5cca5009
Midori <50433979+natsuk4ze@users.noreply.github.com>
2023-06-19 12:11:26 +0900

    Add ios test (#13)
    * Remove unused pub
    
    * Add ios test

--- example/integration_test/integration_test.dart
+++ example/integration_test/integration_test.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
@@ -29,6 +31,8 @@ void main() {
       await tester.tap(button);
       expect(tester.takeException(), isNull);
     });
+
+    if (Platform.isIOS) return;
 
     testWidgets('putVideo()', (tester) async {
       app.main();
