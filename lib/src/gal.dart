675393598dbd41cdef6ef1e4d57316cdd6ce8064
yuoohama <negotoyolo@gmail.com>
2023-07-07 20:12:37 +0900

    Update doc comments of main class

--- lib/src/gal.dart
+++ lib/src/gal.dart
@@ -4,51 +4,51 @@ import 'package:gal/src/gal_exception.dart';
 
 import 'gal_platform_interface.dart';
 
-/// For detailed please see
-/// https://github.com/natsuk4ze/gal/wiki
-/// these functions are first called,
-/// [putImage],[putVideo],[putImageBytes],a native dialog is
-/// called asking the use for permission. If the user chooses to deny,
-/// [GalException] of [GalExceptionType.accessDenied] will be throwed.
-/// You should either do error handling or call [requestAccess] once
-/// before calling these function.
+/// A main class of gal.
+///
+/// See: [wiki](https://github.com/natsuk4ze/gal/wiki)
 @immutable
 final class Gal {
   const Gal._();
 
-  /// Save video to standard gallery app
-  /// [path] is local path.
+  /// Save a video to the gallery from file [path].
+  ///
+  /// Throws an [GalException] If you do not have access premission or
+  /// if an error occurs during saving.
+  /// See: [Formats](https://github.com/natsuk4ze/gal/wiki/Formats)
   static Future<void> putVideo(String path) async =>
       _voidOrThrow(() async => GalPlatform.instance.putVideo(path));
 
-  /// Save image to standard gallery app
-  /// [path] is local path.
+  /// Save a image to the gallery from file [path].
+  ///
+  /// Throws an [GalException] If you do not have access premission or
+  /// if an error occurs during saving.
+  /// See: [Formats](https://github.com/natsuk4ze/gal/wiki/Formats)
   static Future<void> putImage(String path) async =>
       _voidOrThrow(() async => GalPlatform.instance.putImage(path));
 
-  /// Save image to standard gallery app
-  /// [Uint8List] version of [putImage]
-  /// It does not require temporary files and saves directly from memory,
-  /// making it fast.
+  /// Save a image to the gallery from [Uint8List].
+  ///
+  /// Save a image directly from memory without using a temporary file.
+  /// Throws an [GalException] If you do not have access premission or
+  /// if an error occurs during saving.
+  /// See: [Formats](https://github.com/natsuk4ze/gal/wiki/Formats)
   static Future<void> putImageBytes(Uint8List bytes) async =>
       _voidOrThrow(() async => GalPlatform.instance.putImageBytes(bytes));
 
-  /// Open OS standard gallery app.
-  /// Open "iOS Photos" when iOS, "Google Photos" or something when Android.
+  /// Open the default gallery app.
   static Future<void> open() async => GalPlatform.instance.open();
 
-  /// Returns whether or not the app has access.
-  /// For Android API >=29, or <23, this request is unnecessary and returns true.
-  /// For details please see
-  /// https://github.com/natsuk4ze/gal/wiki/Permissions
+  /// Check if the app has access permissions.
+  /// 
+  /// See: [Permissions](https://github.com/natsuk4ze/gal/wiki/Permissions)
   static Future<bool> hasAccess() async => GalPlatform.instance.hasAccess();
 
-  /// Displays a dialog for the request and returns whether the user has accepted.
-  /// If access was already granted, the dialog is not displayed
-  /// and returns true.
-  /// On iOS, once the user rejects it, the dialog cannot be displayed and
-  /// returns false immediately.
-  /// For Android API >=29, or <23, this request is unnecessary and returns true.
+  /// Request access permissions.
+  /// 
+  /// Returns [true] if access is granted, [false] if denied.
+  /// If access was already granted, the dialog is not displayed and returns true.
+  /// See: [Permissions](https://github.com/natsuk4ze/gal/wiki/Permissions)
   static Future<bool> requestAccess() async =>
       GalPlatform.instance.requestAccess();
 
