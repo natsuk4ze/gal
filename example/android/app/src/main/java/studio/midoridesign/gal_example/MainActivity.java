81581872348d18d7125f6cf6c2ba06ca556d1b29
yuoohama <negotoyolo@gmail.com>
2023-07-08 17:46:34 +0900

    Set up kotlin

--- example/android/app/src/main/java/studio/midoridesign/gal_example/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package studio.midoridesign.gal_example;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
---
+++ example/android/app/src/main/kotlin/studio/midoridesign/gal_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package studio.midoridesign.gal_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
