7aef45e2e11212c33b081e526d088a6b1cd39c3f
Midori <50433979+natsuk4ze@users.noreply.github.com>
2023-08-03 21:16:46 +0900

    Refactor

--- android/src/main/java/studio/midoridesign/gal/GalPlugin.java
+++ android/src/main/java/studio/midoridesign/gal/GalPlugin.java
@@ -36,7 +36,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.ByteArrayInputStream;
-
 import java.util.UUID;
 
 public class GalPlugin implements FlutterPlugin, MethodCallHandler, ActivityAware,
