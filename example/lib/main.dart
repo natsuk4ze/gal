d796685264043f6c03e6c316102483779a427f4d
natsuk4ze <natsukaze.midori@gmail.com>
2023-09-03 12:38:55 +0900

    Remove unnecessary Builder

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -25,87 +25,85 @@ class _AppState extends State<App> {
     return MaterialApp(
       navigatorKey: navigatorKey,
       home: Scaffold(
-        body: Builder(builder: (context) {
-          return Center(
-            child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  const Text('toAlbum'),
-                  Switch(
-                      value: toAlbum,
-                      onChanged: (_) => setState(() => toAlbum = !toAlbum)),
-                  FilledButton(
-                    onPressed: () async => Gal.open(),
-                    child: const Text('Open Gallery'),
-                  ),
-                  FilledButton(
-                    onPressed: () async {
-                      final path = await getFilePath('assets/done.mp4');
-                      await Gal.putVideo(path, album: album);
-                      showSnackbar();
-                    },
-                    child: const Text('Save Video from file path'),
-                  ),
-                  FilledButton(
-                    onPressed: () async {
-                      final path = await getFilePath('assets/done.jpg');
-                      await Gal.putImage(path, album: album);
-                      showSnackbar();
-                    },
-                    child: const Text('Save Image from file path'),
-                  ),
-                  FilledButton(
-                    onPressed: () async {
-                      final bytes = await getBytesData('assets/done.jpg');
-                      await Gal.putImageBytes(bytes, album: album);
-                      showSnackbar();
-                    },
-                    child: const Text('Save Image from bytes'),
-                  ),
-                  FilledButton(
-                    onPressed: () async {
-                      final path = '${Directory.systemTemp.path}/done.jpg';
-                      await Dio().download(
-                        'https://github.com/natsuk4ze/gal/raw/main/example/assets/done.jpg',
-                        path,
-                      );
-                      await Gal.putImage(path);
-                      showSnackbar();
-                    },
-                    child: const Text('Download Image'),
-                  ),
-                  FilledButton(
-                    onPressed: () async {
-                      final path = '${Directory.systemTemp.path}/done.mp4';
-                      await Dio().download(
-                        'https://github.com/natsuk4ze/gal/raw/main/example/assets/done.mp4',
-                        path,
-                      );
-                      await Gal.putVideo(path);
-                      showSnackbar();
-                    },
-                    child: const Text('Download Video'),
-                  ),
-                  FilledButton(
-                    onPressed: () async {
-                      final hasAccess = await Gal.hasAccess(toAlbum: toAlbum);
-                      log('Has Access:${hasAccess.toString()}');
-                    },
-                    child: const Text('Has Access'),
-                  ),
-                  FilledButton(
-                    onPressed: () async {
-                      final requestGranted =
-                          await Gal.requestAccess(toAlbum: toAlbum);
-                      log('Request Granted:${requestGranted.toString()}');
-                    },
-                    child: const Text('Request Access'),
-                  ),
-                ],
-              ),
+        body: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                const Text('toAlbum'),
+                Switch(
+                    value: toAlbum,
+                    onChanged: (_) => setState(() => toAlbum = !toAlbum)),
+                FilledButton(
+                  onPressed: () async => Gal.open(),
+                  child: const Text('Open Gallery'),
+                ),
+                FilledButton(
+                  onPressed: () async {
+                    final path = await getFilePath('assets/done.mp4');
+                    await Gal.putVideo(path, album: album);
+                    showSnackbar();
+                  },
+                  child: const Text('Save Video from file path'),
+                ),
+                FilledButton(
+                  onPressed: () async {
+                    final path = await getFilePath('assets/done.jpg');
+                    await Gal.putImage(path, album: album);
+                    showSnackbar();
+                  },
+                  child: const Text('Save Image from file path'),
+                ),
+                FilledButton(
+                  onPressed: () async {
+                    final bytes = await getBytesData('assets/done.jpg');
+                    await Gal.putImageBytes(bytes, album: album);
+                    showSnackbar();
+                  },
+                  child: const Text('Save Image from bytes'),
+                ),
+                FilledButton(
+                  onPressed: () async {
+                    final path = '${Directory.systemTemp.path}/done.jpg';
+                    await Dio().download(
+                      'https://github.com/natsuk4ze/gal/raw/main/example/assets/done.jpg',
+                      path,
+                    );
+                    await Gal.putImage(path);
+                    showSnackbar();
+                  },
+                  child: const Text('Download Image'),
+                ),
+                FilledButton(
+                  onPressed: () async {
+                    final path = '${Directory.systemTemp.path}/done.mp4';
+                    await Dio().download(
+                      'https://github.com/natsuk4ze/gal/raw/main/example/assets/done.mp4',
+                      path,
+                    );
+                    await Gal.putVideo(path);
+                    showSnackbar();
+                  },
+                  child: const Text('Download Video'),
+                ),
+                FilledButton(
+                  onPressed: () async {
+                    final hasAccess = await Gal.hasAccess(toAlbum: toAlbum);
+                    log('Has Access:${hasAccess.toString()}');
+                  },
+                  child: const Text('Has Access'),
+                ),
+                FilledButton(
+                  onPressed: () async {
+                    final requestGranted =
+                        await Gal.requestAccess(toAlbum: toAlbum);
+                    log('Request Granted:${requestGranted.toString()}');
+                  },
+                  child: const Text('Request Access'),
+                ),
+              ],
             ),
-          );
-        }),
+          ),
+        ),
       ),
     );
   }
