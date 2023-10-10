efa83a6473648d6dd2844b80fb2752f7c4c4040c
liyuanbo <lyb5834@126.com>
2023-10-10 17:42:05 +0800

    Fix bugs on Mac
    Fix bugs on Mac

--- darwin/Classes/Gal.h
+++ darwin/Classes/Gal.h
@@ -5,7 +5,11 @@
 //  Created by liyuanbo on 2023/10/10.
 //
 
+#if TARGET_OS_IOS
 #import <Flutter/Flutter.h>
+#else
+#import <FlutterMacOS/FlutterMacOS.h>
+#endif
 
 @interface Gal : NSObject <FlutterPlugin>
 
