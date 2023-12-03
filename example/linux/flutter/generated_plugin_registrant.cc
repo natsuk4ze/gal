2385057e656867b869d90c3950ad653b419fef9d
Ellet <hello@freshplatform.net>
2023-12-03 16:51:50 +0300

    Update pubspec.yaml

--- example/linux/flutter/generated_plugin_registrant.cc
+++ example/linux/flutter/generated_plugin_registrant.cc
@@ -6,6 +6,10 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <gal/gal_plugin.h>
 
 void fl_register_plugins(FlPluginRegistry* registry) {
+  g_autoptr(FlPluginRegistrar) gal_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "GalPlugin");
+  gal_plugin_register_with_registrar(gal_registrar);
 }
